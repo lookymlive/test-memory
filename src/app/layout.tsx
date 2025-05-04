6b38be71daf8e59afd88fd451b20ce563143120e
lookymlive <lookymlive@gmail.com>
2025-05-04 19:15:08 -0300

    Add RootLayout component for Speed Memory application, integrating Navbar and ThemeProvider, and setting up metadata for the application.

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import { Navbar } from "@/components/navbar";
+import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Speed Memory",
+  description: "Entrena y compite en pruebas de memoria",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="relative flex min-h-screen flex-col">
+            <Navbar />
+            <main className="flex-1">{children}</main>
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
