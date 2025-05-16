eaaf0e86b74d9ff4167e6137c47b3cc911583170
lookymlive <lookymlive@gmail.com>
2025-05-16 11:05:37 -0300

    Remove Cloudflare Pages middleware for request routing

--- functions/_middleware.js
+++
@@ -1,5 +0,0 @@
-// This middleware helps route Next.js requests properly on Cloudflare Pages
-export async function onRequest(context) {
-  // Just pass through the request to the Next.js app
-  return await context.next();
-}
